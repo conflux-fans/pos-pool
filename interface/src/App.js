af0dd81fb4158066ec66a112b6bca26ea0baa19c
chao.zhou <zctocm@gmail.com>
2021-11-02 15:56:05 +0800

    feat: init the project

---
+++ interface/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Pos Pool Interface
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
