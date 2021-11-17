9563870fd3bc1203e942630c46d49b3d5de3bbe1
PanaW <pana.wang@outlook.com>
2021-11-17 10:30:28 +0800

    include script update

--- contract/scripts/check-reward.js
+++ contract/scripts/check-reward.js
@@ -1,18 +1,23 @@
 const { conflux } = require("./conflux");
 
-const address =
+let address =
   "NET8888:TYPE.CONTRACT:ACCHHA25SM4P7C0WXC778MMBTVNE99X53PXFWE26UN";
+address = "NET8888:TYPE.CONTRACT:ACFANGB2WPPT9YB1U4F4272BYZF6NUWJ3PFJ9MTFGZ";
+
+if (process.argv.length > 2) {
+  address = process.argv[2];
+}
 
 async function main() {
   const status = await conflux.pos.getStatus();
-  console.log("PoS status: ", status);
+  console.log("Latest PoS Status: ", status);
 
   for (let i = status.epoch; i >= 0; i--) {
     const reward = await conflux.pos.getRewardsByEpoch(i);
     if (!reward) continue;
     for (const item of reward.accountRewards) {
       if (item.powAddress === address) {
-        console.log(item);
+        console.log(`Epoch ${i}: `, item);
       }
     }
   }
--- contract/scripts/deploy-pos-pool.js
+++ contract/scripts/deploy-pos-pool.js
@@ -17,12 +17,15 @@ poolManagerAddress = "NET8888:TYPE.CONTRACT:ACC7GGWT2M5D8VY6F9P9221TN8KB4CRH2UVW
 
 const registData = '0xe335b4519ec6302aca39b1f58375f0e835e4ac7472618f2fef7b4cdefa1e657683881f25000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000a00000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000016000000000000000000000000000000000000000000000000000000000000000308ff838a518ea03260c912ffc8b38a74467a23218c2042110b8e0a740eebd5a99036383fed6207dc3fbf0f1784f18f796000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000021021c1a2b1a0367dd5bc13ebf961389f6bbaa76de02b76a6b7ae44e9ef585e8033400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000a00000000000000000000000000000000000000000000000000000000000000030a3d6dd2086e7e27a6c6a5d05960ad6b814528ee42ce7bf9ee1dd96e307f36eeec78399d8b03049d8b7b5b389503e517e00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002080d2cef79f9b0618109f59dd0f3201ebebfae766a83577b0742658cbd0eb9646';
 // 9ec6302aca39b1f58375f0e835e4ac7472618f2fef7b4cdefa1e657683881f25
+const mockPosRegisterAddress = 'NET8888:TYPE.CONTRACT:ACGMCGJFNJ1SSPHC1497RKC1VW6N7Z5D1J5TU8KN4D';
+const mockStakingAddress = 'NET8888:TYPE.CONTRACT:ACF4KU7SD1B3YMUTESMYAFGZDJDKP39PBAT9AKU8JM';
+const posPoolDebugAddress = 'NET8888:TYPE.CONTRACT:ACBZXYTA8P9WSH6GJXZTXPFMMWS6J0ACJJG6XKR7W3';
 
 /*
+  0. 部署矿池 Manager 合约
   1. 部署矿池合约
-  2. 部署矿池Manager合约
+  2. 对矿池进行注册
   3. 将矿池合约添加到矿池Manager合约
-  4. 对矿池进行注册
 */
 
 async function main() {
@@ -32,20 +35,31 @@ async function main() {
     address: poolAddress,
   });
 
+  const poolContractDebug = conflux.Contract({
+    abi,
+    address: posPoolDebugAddress,
+  });
+
   const managerContract = conflux.Contract({
     abi: poolManagerInfo.abi,
     address: poolManagerAddress,
   });
   // poolAddress = await deployPool();
   // poolManagerAddress = await deployPoolManager();
-  // await addPoolToManager(managerContract, poolAddress);
-  // await setPoolName(poolContract, "TestOne");
+  // await addPoolToManager(managerContract, posPoolDebugAddress);
+  // await setPoolName(poolContractDebug, "PoSPoolDebug");
+  // await setPoolLockPeriod(poolContractDebug, 2 * 60 * 30);
   // await getPools(managerContract);
   // await getPoolInfo(poolContract);
-  // await registerPool(poolAddress, registData);
+  await registerPool(posPoolDebugAddress, registData);
+  // await setCfxCountOfOneVote(poolContractDebug, 100);
   // await increaseStake(poolContract, 1);
 
   // await increaseStake(poolContract, 601);
+
+  // await deployMockPosRegister();
+  // await deployMockStaking();
+  // await deployPoolDebug(mockStakingAddress, mockPosRegisterAddress);
 }
 
 async function getPoolInfo(contract) {
@@ -67,6 +81,22 @@ async function setPoolName(contract, name) {
   _checkReceipt(result);
 }
 
+async function setPoolLockPeriod(contract, period) {
+  console.log("==================== Set pool lockPeriod");
+  let result = await contract.setLockPeriod(period).sendTransaction({
+    from: account.address
+  }).executed();
+  _checkReceipt(result);
+}
+
+async function setCfxCountOfOneVote(contract, count) {
+  console.log("==================== Set pool cfxCountOfOneVote");
+  let result = await contract.setCfxCountOfOneVote(count).sendTransaction({
+    from: account.address
+  }).executed();
+  _checkReceipt(result);
+}
+
 async function increaseStake(contract, votePower) {
   console.log("==================== increase stake");
   const receipt = await contract.increaseStake(votePower).sendTransaction({
@@ -83,7 +113,6 @@ async function getPools (contract) {
 }
 
 async function deployContract(abi, bytecode, ...args) {
-  console.log("==================== Deploying contract");
   const contract = conflux.Contract({
     abi,
     bytecode,
@@ -145,13 +174,13 @@ async function addPoolToManager(contract, poolAddress) {
   console.log("Pool added to manager result: ", receipt.outcomeStatus);
 }
 
-async function registerPool(poolAddress, data) {
+async function registerPool(poolAddress, data, value = 1000) {
   console.log("=================== Register pool");
   const receipt = await conflux.sendTransaction({
     from: account.address,
     to: poolAddress,
     data,
-    value: Drip.fromCFX(1000),
+    value: Drip.fromCFX(value),
   }).executed();
 
   console.log("Pool register result: ", receipt.outcomeStatus);
