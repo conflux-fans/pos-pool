76342550e784cc69b5ccc7460e04093d5221ffa2
W <pana.wang@outlook.com>
2021-12-23 15:30:36 +0800

    Add monitor scripts  (#24)
    * Add more detail in readme
    
    * add monit scripts
    
    * update monitor scripts
    
    * update withdraw method

--- contract/scripts/monitor.js
+++ contract/scripts/monitor.js
@@ -2,16 +2,25 @@
 /* eslint-disable no-unneeded-ternary */
 // This is a monitor script used to check whether a PoS node is forceRetired.
 // If the node is forceRetired, this script will push Dingding message to notify the Manager.
-const {conflux} = require("./conflux");
+const { conflux } = require("./conflux");
 const posNodeAddresses = require('./pos-node-addresses.json');
+const { dingAlert } = require('./dingAlert');
 
 async function main() {
   setInterval(async () => {
     for (const posNodeAddress of posNodeAddresses) {
-      const forceRetired = await checkForceRetired(posNodeAddress);
-      console.log(`${(new Date()).toLocaleString()} is ${posNodeAddress} forceRetired ? : ${forceRetired}`);
+      try {
+        const forceRetired = await checkForceRetired(posNodeAddress);
+        const message = `${(new Date()).toLocaleString()} is ${posNodeAddress} forceRetired? : ${forceRetired}`;
+        console.log(message);
+        if (forceRetired) {
+          dingAlert(message);
+        }
+      } catch (err) {
+        console.log('Check failed: ', err);
+      }
     }
-  }, 1000 * 60);
+  }, 1000 * 60);  // check every 1 minute
 }
 
 main().catch(console.log);
