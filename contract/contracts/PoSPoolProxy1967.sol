0e962f7a43bad3e6ca1ec40665273a2849a10b62
PanaW <pana.wang@outlook.com>
2021-12-25 17:32:06 +0800

    use new way to release the pool

--- contract/contracts/PoSPoolProxy1967.sol
+++ contract/contracts/PoSPoolProxy1967.sol
@@ -4,7 +4,7 @@ import "./PoSPoolStorage.sol";
 import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PoSPoolProxy1967 is PoSPoolStorage, ERC1967Proxy, Ownable {
+contract PoSPoolProxy1967 is ERC1967Proxy, Ownable {
   constructor(address logic) ERC1967Proxy(logic, "") {}
 
   // Used to query current logic address
--- contract/contracts/PoolContext.sol
+++ contract/contracts/PoolContext.sol
@@ -6,12 +6,11 @@ pragma solidity ^0.8.0;
 
 abstract contract PoolContext {
   function _selfBalance() internal view virtual returns (uint256) {
-    address self = address(this);
-    return self.balance;
+    return address(this).balance;
   }
 
-  function _blockNumber() internal view virtual returns (uint64) {
-    return uint64(block.number);
+  function _blockNumber() internal view virtual returns (uint256) {
+    return block.number;
   }
 
   Staking private constant STAKING = Staking(0x0888000000000000000000000000000000000002);
