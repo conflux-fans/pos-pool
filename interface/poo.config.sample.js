01e5dc83cccf4a2e39148379b510901e6636455c
PanaW <pana.wang@outlook.com>
2022-02-18 11:35:01 +0800

    add three new language and create a frontend config

---
+++ interface/poo.config.sample.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  defaultLang: 'en',
+  testnet: {
+    poolManagerAddress: 'cfxtest:xxxxxxxxxxxxxxxxxxxxxxxxxx',
+    RPC: 'https://test.confluxrpc.com',
+    networkId: 1
+  },
+  mainnet: {
+    poolManagerAddress: 'cfx:xxxxxxxxxxxxxxxxxxxxxx',
+    RPC: 'https://main.confluxrpc.com',
+    networkId: 1029
+  }
+}
