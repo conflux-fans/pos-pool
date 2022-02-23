317c631ba7c0acb1cfb2df82efc8c19fa7f3b432
PanaW <pana.wang@outlook.com>
2022-02-23 17:41:06 +0800

    remove commented code

--- contract/scripts/dingAlert.js
+++ contract/scripts/dingAlert.js
@@ -9,7 +9,6 @@ async function dingAlert(message) {
     throw new Error('DING_ROBOT_TOKEN is not set');
   }
   const robot = new DingRobot(token);
-  // robot.atAll(true).text('大家好！我是 ding-robot！');
   robot.text('PoSNode: ' + message);
 }
 
