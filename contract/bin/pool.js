87f0ec2937f91fc6248d9442a412fb7619e6767b
PanaW <pana.wang@outlook.com>
2022-04-26 19:29:17 +0800

    update account init method

--- contract/bin/pool.js
+++ contract/bin/pool.js
@@ -36,7 +36,14 @@ const conflux = new Conflux({
   networkId: parseInt(process.env.CFX_NETWORK_ID),
   // logger: console,
 });
-const account = conflux.wallet.addPrivateKey(process.env.PRIVATE_KEY);
+
+let account;
+if (process.env.PRIVATE_KEY) {
+  account = conflux.wallet.addPrivateKey(process.env.PRIVATE_KEY);
+} else {
+  const keystore = require(process.env.KEYSTORE);
+  account = conflux.wallet.addKeystore(keystore, process.env.KEYSTORE_PWD);
+}
 
 // const posRegisterContract = conflux.InternalContract('PoSRegister');
 
--- contract/scripts/conflux.js
+++ contract/scripts/conflux.js
@@ -7,7 +7,14 @@ const conflux = new Conflux({
   networkId: parseInt(process.env.CFX_NETWORK_ID),
 });
 
-const account = conflux.wallet.addPrivateKey(process.env.PRIVATE_KEY);
+let account;
+if (process.env.PRIVATE_KEY) {
+  account = conflux.wallet.addPrivateKey(process.env.PRIVATE_KEY);
+} else {
+  const keystore = require(process.env.KEYSTORE);
+  account = conflux.wallet.addKeystore(keystore, process.env.KEYSTORE_PWD);
+}
+
 
 module.exports = {
   conflux,
