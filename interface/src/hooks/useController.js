6858a84b444e058563b70646a0dfe3b4415c2fbb
SSSensational <sss18201672034@163.com>
2022-04-30 23:09:02 +0800

    fix: eSpace ethers use provider insteadof signer

--- interface/src/hooks/useController.js
+++ interface/src/hooks/useController.js
@@ -3,16 +3,15 @@ import {Conflux} from "js-conflux-sdk/dist/js-conflux-sdk.umd.min.js";
 import useCurrentNetwork from './useCurrentNetwork';
 import useCurrentSpace from './useCurrentSpace';
 import { ethers, Contract} from 'ethers';
-const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
-const signer = provider.getSigner();
 
 const useController = () => {
     const currentSpace = useCurrentSpace();
     const currentNetwork = useCurrentNetwork();
     return useMemo(() => {
         if (currentSpace === 'eSpace') {
+            const provider = new ethers.providers.JsonRpcProvider(currentNetwork.url);
             return {
-                Contract: ({ abi, address }) => new Contract(address, abi, signer),
+                Contract: ({ abi, address }) => new Contract(address, abi, provider),
                 provider
             }
         }
